38bb0c1d418499c50c6887a1b4c7f2ca074638d9
azapata13 <z.felipealberto@gmail.com>
2026-09-21 18:43:29 -0400

    Separate Helena and Molina web apps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        helena: fileURLToPath(new URL('./index.html', import.meta.url)),
+        molina: fileURLToPath(new URL('./molina/index.html', import.meta.url)),
+      },
+    },
+  },
 })
